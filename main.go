2e8ca3c256104c97db3ab71698ded406b56f54e5
bangora60 <bangora60@gmail.com>
2023-12-15 00:35:46 +0000

    second task

--- main.go
+++ main.go
@@ -3,51 +3,31 @@ package main
 import (
 	"context"
 	"fmt"
-	"golang.org/x/sync/errgroup"
+	"github.com/bangora60/go_todo_app/config"
 	"log"
 	"net"
-	"net/http"
 	"os"
 )
 
-func run(ctx context.Context, l net.Listener) error {
-	s := &http.Server{
-		Addr: ":18080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
+func main() {
+	if err := run(context.Background()); err != nil {
+		log.Printf("failed to terminated server: %v", err)
+		os.Exit(1)
 	}
-	eg, ctx := errgroup.WithContext(ctx)
-
-	eg.Go(func() error {
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	return eg.Wait()
 }
 
-func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-
-	p := os.Args[1]
-	l, err := net.Listen("tcp", ":"+p)
+func run(ctx context.Context) error {
+	cfg, err := config.New()
 	if err != nil {
-		log.Fatalf("failed to listen port %s: %v", p, err)
+		return err
 	}
-	if err := run(context.Background(), l); err != nil {
-		log.Printf("failed to terminate server: %v", err)
-		os.Exit(1)
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
+	if err != nil {
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
+	url := fmt.Sprintf("http://%s", l.Addr().String())
+	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
